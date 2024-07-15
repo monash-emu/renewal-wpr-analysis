bac5ffe8e0200472c7f8990e099bab18abb6d6cb
David Shipman <dshipman@gmail.com>
2024-07-15 13:44:45 +1000

    Update paths in utils

--- renewal_analysis/utils.py
+++ renewal_analysis/utils.py
@@ -1,12 +1,12 @@
 import pandas as pd
 from pathlib import Path
 
-cwd = Path.cwd()
+__base_path = Path(__file__).parent
 
 # Function for loading target data
 def load_target_data(country):
-    cwd = Path.cwd()
-    target_data_path = cwd.parent / Path("data/target-data/case-data.csv")
+    
+    target_data_path = __base_path / "data/target-data/case-data.csv"
     if country == 'Malaysia':
         target_data = pd.read_csv(target_data_path, index_col=0)['New_cases_MYS']
     elif country == 'Philippines':
@@ -17,8 +17,7 @@ def load_target_data(country):
 
 # Function for loading google mobility data
 def load_mobility_data(country):
-    cwd = Path.cwd()
-    mobility_data_path = cwd.parent / Path("data/mobility-data/mobility-data.csv")
+    mobility_data_path = __base_path / Path("data/mobility-data/mobility-data.csv")
     mobility_data = pd.read_csv(mobility_data_path, index_col=0)
     mobility_data = mobility_data[mobility_data["country_region"] == country]
     mobility_data.index = pd.to_datetime(mobility_data.index)
@@ -26,16 +25,15 @@ def load_mobility_data(country):
 
 # function for loading variant prevalence data
 def load_variant_prevalence_data(country):
-    cwd = Path.cwd()
-    variant_data_path = cwd.parent / Path("data/variant-data/variant-prevalence.csv")
+    variant_data_path = __base_path / Path("data/variant-data/variant-prevalence.csv")
     variant_data = pd.read_csv(variant_data_path, index_col=0)
     variant_data = variant_data[variant_data["country"] == country]
     return variant_data
 
 # function for loading vaccination data
 def load_vaccination_data(country):
-    cwd = Path.cwd()
-    vaccination_data_path = cwd.parent / Path("data/vaccination-data/vaccination-data.csv")
+    __base_path
+    vaccination_data_path = __base_path / Path("data/vaccination-data/vaccination-data.csv")
     vaccination_data = pd.read_csv(vaccination_data_path, index_col=0)
     vaccination_data = vaccination_data[vaccination_data["location"] == country]
     vaccination_data.index = pd.to_datetime(vaccination_data.index)
